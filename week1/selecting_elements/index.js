aa27694cd2ea9f45cef0c2d07709f475461a0944
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2021-08-06 01:14:31 -0500

    Selecting elements of the DOM assignment

---
+++ week1/selecting_elements/index.js
@@ -0,0 +1,2 @@
+//Add text to the h1 with tag 'header1' of index.html file.
+document.getElementById("header").innerHTML = "This is my assignment for selecting elements in the document object model";
