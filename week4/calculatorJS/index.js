371b90c23b2d7f7d6cf2529f141504c57571c22d
Easton Yarbrough <easton.yarbrough@bryanuniversity.edu>
2021-08-24 22:43:36 -0500

    Revised based on instructor comments

--- week4/calculatorJS/index.js
+++ week4/calculatorJS/index.js
@@ -16,15 +16,15 @@ function multiplication(num1, num2)
 
 //Addition.
 document.getElementById("addButton").addEventListener("click", function() {
-    document.getElementById("addResult").textContent = addition(Number(document.getElementById("addFirst").value), Number(document.getElementById("addSecond").value));
+    document.getElementById("result").textContent = addition(Number(document.getElementById("addFirst").value), Number(document.getElementById("addSecond").value));
 });
 
 //Subtraction.
 document.getElementById("subButton").addEventListener("click", function() {
-    document.getElementById("subResult").textContent = subtraction(Number(document.getElementById("subFirst").value), Number(document.getElementById("subSecond").value));
+    document.getElementById("result").textContent = subtraction(Number(document.getElementById("subFirst").value), Number(document.getElementById("subSecond").value));
 });
 
 //Multiplication.
 document.getElementById("mulButton").addEventListener("click", function() {
-    document.getElementById("mulResult").textContent = multiplication(Number(document.getElementById("mulFirst").value), Number(document.getElementById("mulSecond").value));
+    document.getElementById("result").textContent = multiplication(Number(document.getElementById("mulFirst").value), Number(document.getElementById("mulSecond").value));
 });
