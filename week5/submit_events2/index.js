7a24f56747c965d2fbee29b40a378eb87bf80f4c
eastonyarbrough <easton.yarbrough@bryanuniversity.edu>
2021-09-02 00:26:08 -0500

    Complete 'Using other types of HTML outputs' assignment

--- week5/submit_events2/index.js
+++ week5/submit_events2/index.js
@@ -8,6 +8,18 @@ let hurd = form.elements["hurdyDurdy9K"];
 let burp = form.elements["burpleTurple"];
 let grog = form.elements["grognakSnack"];
 
+hurd.addEventListener("click", function() {
+    hurd.setAttribute("value", "HurdyDurdy9000");
+});
+
+burp.addEventListener("click", function() {
+    burp.setAttribute("value", "BurpleTurple");
+});
+
+grog.addEventListener("click", function() {
+    grog.setAttribute("value", "GrognakSnack");
+});
+
 form.addEventListener("submit", function() {
-    alert(firstName.value + lastName.value + age.value + enjoy.value + experience.value + hurd.value + burp.value + grog.value);
+    alert("First Name: " + firstName.value + " / Last Name: " + lastName.value + " / Age: " + age.value + " / Did you enjoy our product?: " + enjoy.value + " / How was your experience?: " + experience.value + " / Which product(s) did you purchase?: " + hurd.value + " " + burp.value + " " + grog.value);
 });
